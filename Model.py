a20230aa734e409d57f9dd02dd3b17b663227a47
Josh Barua <joshbarua2002@gmail.com>
2023-02-10 14:02:26 -0800

    created generator and discriminator network

--- Model.py
+++ Model.py
@@ -10,15 +10,105 @@ from torchvision.utils import save_image
 from torchvision.utils import make_grid
 from torch.utils.tensorboard import SummaryWriter 
 
-#loading and preprocessing data
+# loading and preprocessing data
 
-#resize images to fixed size, convert image to tensor, normalize pixel image from [0, 255] to [-1, 1]
+# resize images to fixed size, convert image to tensor, normalize pixel image from [0, 255] to [-1, 1]
 train_transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor(), transforms.Normalize([.5, .5, .5], [.5, .5, .5])])
 
-#import data and apply transformations
+# import data and apply transformations
 train_data = datasets.ImageFolder(root='faces', transform=train_transform)
 
-#create iterable over faces dataset
+# create iterable over faces dataset
 train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+
+# custom weights initialization function for generator and discriminator layers
+def weights_init(m):
+    classname = m.__class__.__name__
+
+    # convolutional layers weights initialized from normal dist with mean: 0, std: .02
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+
+    # batch-normalization layer weights initialized from normal dist with mean: 1, std: .02, and 0 bias
+    elif classname.find('BatchNorm') != -1:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)  
+
+# generator network 
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+
+            # block 1: input is Z, going into a convolution
+            nn.ConvTranspose2d(latent_dim, 64 * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(64 * 8),
+            nn.ReLU(True),
+
+            # block 2: input is (64 * 8) x 4 x 4
+            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.ReLU(True),
+
+            # block 3: input is (64 * 4) x 8 x 8
+            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.ReLU(True),
+
+            # block 4: input is (64 * 2) x 16 x 16
+            nn.ConvTranspose2d(64 * 2, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            # block 5: input is (64) x 32 x 32
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+
+            # output: output is (3) x 64 x 64
+        )
+    
+    # forward function which is fed the noise vector
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
+# discriminator network
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+
+            # block 1: input is (3) x 64 x 64
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # block 2: input is (64) x 32 x 32
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # block 3: input is (64*2) x 16 x 16
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # block 4: input is (64*4) x 8 x 8
+            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # block 5: input is (64*8) x 4 x 4
+            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid(),
+            nn.Flatten()
+
+            # output: 1
+        )
+
+    # forward function which is fed generated image and predicts (1) real or (0) fake
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
 
 
